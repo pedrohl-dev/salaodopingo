b2eba740bf5f5caf4ae1ba4a592e34ba3b14e1a3
Pedro Henrique <pedrolimapedrolima100@gmail.com>
2026-05-19 19:26:04 +0000

    Corrigindo 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
 import "./styles.css";
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+if (redirect) {
+  window.history.replaceState(null, "", "/salaodopingo" + redirect);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />

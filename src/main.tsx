75f4f3d5dbdb21c6a6814bbb370193c4d661d003
Pedro Henrique <pedrolimapedrolima100@gmail.com>
2026-05-19 15:52:56 -0300

    Remove unused router import from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(

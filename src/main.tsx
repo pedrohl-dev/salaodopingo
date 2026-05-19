d5e4a23660e827d107383caf7032406347681555
Pedro Henrique <pedrolimapedrolima100@gmail.com>
2026-05-19 15:59:19 -0300

    Replace RouterProvider with Hello World div

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div>Hello World</div>
   </React.StrictMode>
 );

39685a1e31d3a9128d3dd187f59e7d981d97f4aa
Pedro Henrique <pedrolimapedrolima100@gmail.com>
2026-05-19 19:37:34 +0000

    fix: hash routing para router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,14 @@
-import { createRouter } from "@tanstack/react-router";
+import { createRouter, createHashHistory } from "@tanstack/react-router";
 import { QueryClient } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 
 export const queryClient = new QueryClient();
 
+const hashHistory = createHashHistory();
+
 export const router = createRouter({
   routeTree,
+  history: hashHistory,
   context: {
     queryClient,
   },
